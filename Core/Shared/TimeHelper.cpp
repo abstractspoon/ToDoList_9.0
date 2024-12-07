096053d3104b9a824d6d7706acabee4c8b8a49e9
abstractspoon <abstractspoon@users.noreply.github.com>
2024-12-07 16:33:40 +1100

    Don't promote time units when HMS formatting

--- Core/Shared/TimeHelper.cpp
+++ Core/Shared/TimeHelper.cpp
@@ -422,6 +422,21 @@ CString CTimeHelper::FormatTimeHMS(double dTime, TH_UNITS nUnitsFrom, DWORD dwFl
 	
 	CString sTime;
 	
+	if (dwFlags & HMS_PRESERVEUNITS)
+	{
+		// Clear values corresponding to units 'higher' than 'nUnitsFrom'
+		switch (nUnitsFrom)
+		{
+		case THU_MINS:		dHours = 0.0;	// fall through
+		case THU_HOURS:		dDays = 0.0;	// fall through
+		case THU_WEEKDAYS:	dWeeks = 0.0;	// fall through
+		case THU_DAYS:		dWeeks = 0.0;	// fall through
+		case THU_WEEKS:		dMonths = 0.0;	// fall through
+		case THU_MONTHS:	dYears = 0.0;	// fall through
+		case THU_YEARS:		break;
+		}
+	}
+
 	if (dYears >= 1.0)
 	{
 		sTime = FormatTimeHMS(dYears, THU_YEARS, THU_MONTHS, MONTHS2YEARS, bDecPlaces, cDelim);
