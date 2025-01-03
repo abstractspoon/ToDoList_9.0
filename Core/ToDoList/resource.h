d28e2ea1a11b1bb5ddb92390c2db714b8d3a7334
abstractspoon <abstractspoon@users.noreply.github.com>
2025-01-03 12:27:27 +1100

    Increment next ctrl ID

--- Core/ToDoList/resource.h
+++ Core/ToDoList/resource.h
@@ -2557,7 +2557,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        335
 #define _APS_NEXT_COMMAND_VALUE         34771
-#define _APS_NEXT_CONTROL_VALUE         1582
+#define _APS_NEXT_CONTROL_VALUE         1583
 #define _APS_NEXT_SYMED_VALUE           108
 #endif
 #endif
